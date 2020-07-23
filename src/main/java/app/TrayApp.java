c78a4294f5f9306f76ac215be968124c5dc3fa81
daniel <danielnd14@gmail.com>
2020-07-23 18:17:58 -0300

    update logback

--- src/main/java/app/TrayApp.java
+++ src/main/java/app/TrayApp.java
@@ -6,7 +6,7 @@ import dorkbox.systemTray.SystemTray;
 public class TrayApp {
 	public static void main(final String[] args) {
 		SystemTray.DEBUG = true;
-		SystemTray.ENABLE_ROOT_CHECK = false;
+		//SystemTray.ENABLE_ROOT_CHECK = false;
 		new TrayMenu();
 		System.out.println(">>>>>Loaded<<<<<");
 	}
